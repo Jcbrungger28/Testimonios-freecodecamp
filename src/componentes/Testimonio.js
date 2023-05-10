c5e11be47ed6e3a86be167441543c3292f07695c
Baylis Brungger <99766100+Jcbrungger28@users.noreply.github.com>
2023-05-10 18:44:40 -0500

    Se agrega el proyecto

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,29 @@
+
+import '../hojas-de-estilos/Testimonio.css'
+
+
+function Testimonio(props){
+  return(
+    <div className='contenedor-testimonio'>
+     <img 
+     className='imagen-testimonio'
+     src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+     alt={`foto de ${props.nombre}`}/>
+     <div
+     className='contenedor-texto-testimonio'>
+      <p className='nombre-testimonio'>
+        <strong>{props.nombre}</strong> en {props.pais} 
+      </p>
+      <p className='cargo-testimonio'>
+        {props.cargo} en <strong>{props.empresa}</strong>
+      </p>
+      <p className='texto-testiminio'>
+        "{props.testimonio}"
+      </p>
+     </div>
+    </div>
+  ); 
+}
+
+
+export default Testimonio;
